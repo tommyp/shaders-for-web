00262d3e11d40b23ffa2def4b7cc549892dc4ef3
Tommy Palmer <hi@tommyp.org>
2021-12-28 14:15:57 +0000

    hi records

---
+++ hi-records/canvas.js
@@ -0,0 +1,46 @@
+const canvas = document.querySelector("canvas")
+const sandbox = new GlslCanvas(canvas)
+
+const sizer = function () {
+  const s = window.innerWidth
+  const dpi = window.devicePixelRatio
+
+  canvas.width = s * dpi
+  canvas.height = s * dpi
+  canvas.style.width  = s + "px"
+  canvas.style.width  = s + "px"
+  canvas.style.height  = s + "px"
+  
+}
+
+sizer()
+
+window.addEventListener("resize", function () {
+  sizer()
+})
+
+sandbox.load(frag)
+sandbox.setUniform("scroll", 0)
+sandbox.setUniform("innerColors", 
+  [0.977, 0.989, 0.641, 1.0],
+  [0.773, 0.711, 1.000, 1.0],
+  [0.963, 0.649, 0.646, 1.0],
+)
+
+sandbox.setUniform("midColors", 
+  [1.000, 0.713, 0.216, 1.0],
+  [0.730, 0.901, 0.201, 1.0],
+  [0.533, 0.941, 1.000, 1.0],
+)
+
+sandbox.setUniform("outerColors", 
+  [1.000, 0.245, 0.226, 1.0],
+  [0.071, 0.557, 0.300, 1.0],
+  [0.000, 0.206, 0.758, 1.0],
+)
+
+window.addEventListener("scroll", function () {
+  const pixels = window.pageYOffset
+  const wh = window.innerHeight
+  sandbox.setUniform("scroll", pixels/wh)
+})
