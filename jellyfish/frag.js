c3c83bb9060e320bfe40d8168bfd44c0b19ea049
Tommy Palmer <hi@tommyp.org>
2022-01-19 12:13:35 +0000

    finish project

--- jellyfish/frag.js
+++ jellyfish/frag.js
@@ -11,7 +11,7 @@ const frag = `
 
   vec3 addLight(vec3 lightColor, vec3 lightPosition) {
     // ambient color
-    float ambientStrength = 0.8;
+    float ambientStrength = 0.3;
     vec3 ambientColor = ambientStrength * lightColor;
 
     // diffuse color - matte color
@@ -23,8 +23,8 @@ const frag = `
     // specular color - gloss
     vec3 cameraDirection = normalize(cameraPosition - v_position);
     vec3 reflectionDirection = normalize(lightDirection + cameraDirection);
-    float specularStrength = 0.1;
-    float shininess = 6.0;
+    float specularStrength = 5.5;
+    float shininess = 12.0;
     float specularScore = pow(max(dot(reflectionDirection, v_normal), 0.0), shininess);
     vec3 specularColor = specularStrength * specularScore * lightColor;
 
@@ -56,12 +56,12 @@ const frag = `
     vec4 objectColor = vec4(rSample.r, gSample.g, bSample.b, 1.0);
 
     vec3 light1 = addLight(
-      vec3(1.0, 1.0, 1.0),
+      vec3(0.1, 0.1, 1.0),
       vec3(60.0 * cos(time), 60.0 * sin(time), 60.0)
     );
 
     vec3 light2 = addLight(
-      vec3(1.0, 1.0, 1.0),
+      vec3(0.5, 0.1, 0.5),
       vec3(100.0 * cos(time), 100.0 * sin(time), 0.0)
     );
 
