91b4ebe660965d7816460f87d4388564f01d8266
Tommy Palmer <hi@tommyp.org>
2022-01-19 11:32:11 +0000

    change to sphere

--- jellyfish/frag.js
+++ jellyfish/frag.js
@@ -11,7 +11,7 @@ const frag = `
 
   vec3 addLight(vec3 lightColor, vec3 lightPosition) {
     // ambient color
-    float ambientStrength = 0.3;
+    float ambientStrength = 0.8;
     vec3 ambientColor = ambientStrength * lightColor;
 
     // diffuse color - matte color
@@ -23,7 +23,7 @@ const frag = `
     // specular color - gloss
     vec3 cameraDirection = normalize(cameraPosition - v_position);
     vec3 reflectionDirection = normalize(lightDirection + cameraDirection);
-    float specularStrength = 5.5;
+    float specularStrength = 0.5;
     float shininess = 12.0;
     float specularScore = pow(max(dot(reflectionDirection, v_normal), 0.0), shininess);
     vec3 specularColor = specularStrength * specularScore * lightColor;
@@ -35,12 +35,12 @@ const frag = `
     vec4 objectColor = mix(vec4(0.5, 0.5, 0.5, 1.0), textureCube(cube, v_normal), 0.5);
 
     vec3 light1 = addLight(
-      vec3(0.1, 0.1, 1.0),
+      vec3(1.0, 1.0, 1.0),
       vec3(60.0 * cos(time), 60.0 * sin(time), 60.0)
     );
 
     vec3 light2 = addLight(
-      vec3(0.5, 0.1, 0.5),
+      vec3(1.0, 1.0, 1.0),
       vec3(100.0 * cos(time), 100.0 * sin(time), 0.0)
     );
 
