aeac5acb980528d00b991465bc34f267be224fcc
Tommy Palmer <hi@tommyp.org>
2022-01-25 15:07:39 +0000

    set theme when changing slides

--- cloth-studios/slides.js
+++ cloth-studios/slides.js
@@ -2,25 +2,31 @@ const cloths = [
   {
     title: 'Print #1',
     src: 'image1.jpg',
+    theme: '',
   },
   {
     title: 'Print #2',
     src: 'image2.jpg',
+    theme: '',
   },
   {
     title: 'Print #3',
     src: 'image3.jpg',
+    theme: 'cream',
   },
   {
     title: 'Print #4',
     src: 'image4.jpg',
+    theme: 'dark',
   },
   {
     title: 'Print #5',
     src: 'image5.jpg',
+    theme: 'dark',
   },
 ];
 
+const body = document.body;
 const image = document.querySelector('section img');
 const description = document.querySelector('div.description');
 const prevTag = document.querySelector('a.prev');
@@ -40,6 +46,7 @@ const update = function () {
   const cloth = cloths[current];
   description.innerHTML = cloth.title;
   image.setAttribute('src', `./assets/${cloth.src}`);
+  body.className = cloth.theme;
 };
 
 update();
