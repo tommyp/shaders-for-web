0e19038b53fa3c705b31611317032517fb6ed4f6
Tommy Palmer <hi@tommyp.org>
2022-01-27 11:25:20 +0000

    add three.js

--- cloth-studios/slides.js
+++ cloth-studios/slides.js
@@ -27,12 +27,36 @@ const cloths = [
 ];
 
 const body = document.body;
-const image = document.querySelector('section img');
+const section = document.querySelector('section');
 const description = document.querySelector('div.description');
 const prevTag = document.querySelector('a.prev');
 const nextTag = document.querySelector('a.next');
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  section.clientWidth / section.clientHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true,
+});
+renderer.setClearColor(0xff0000, 0);
+renderer.setSize(section.clientWidth, section.clientHeight);
+
+section.appendChild(renderer.domElement);
+
 let current = 0;
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
 
 const next = () => {
   current + 1 > cloths.length - 1 ? (current = 0) : (current += 1);
@@ -45,10 +69,16 @@ const prev = () => {
 const update = function () {
   const cloth = cloths[current];
   description.innerHTML = cloth.title;
-  image.setAttribute('src', `./assets/${cloth.src}`);
+  // image.setAttribute('src', `./assets/${cloth.src}`);
+
   body.className = cloth.theme;
 };
 
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
 update();
 
 prevTag.addEventListener('click', (e) => {
